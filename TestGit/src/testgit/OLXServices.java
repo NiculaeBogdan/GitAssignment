33c9965816411cf23e5e743940042379680f3c38
NiculaeBogdan <62154793+NiculaeBogdan@users.noreply.github.com>
2020-03-14 19:00:51 +0200

    i created a class OLXServices that has 2 private attributes (name and price of the service)
    these attributes describes the best a service from this platform and has a constructor with parameters; every customer want to know the name of the service and also how much it costs.

---
+++ TestGit/src/testgit/OLXServices.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testgit;
+
+/**
+ *
+ * @author bogdann
+ */
+public class OLXServices {
+    private String nameOfTheService;
+    private double priceOfTheService;
+    
+    public OLXServices(String nameOfTheService, double priceOfTheService){
+        this.nameOfTheService = nameOfTheService;
+        this.priceOfTheService = priceOfTheService;
+    }
+    
+}
